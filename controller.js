4aedc37afdeba4e1cab756e6c139c728642abaf8
Juan Mora <raicerk@gmail.com>
2017-02-13 01:30:05 -0300

    Se deja funcional funciones de subscripcion

--- controller.js
+++ controller.js
@@ -2,28 +2,30 @@ var mongoose = require('mongoose');
 var User = mongoose.model('usuarios');
 var satelize = require('satelize');
 var moment = require('moment');
+var requestIp = require('request-ip');
 
 exports.registro = function(req, res) {
 
-    console.log(req.headers['x-forwarded-for']);
-    console.log(req.connection.remoteAddress);
-    console.log(req.socket.remoteAddress);
-    //190.47.115.14
+    try {
+        var clientIp = requestIp.getClientIp(req);
+        var ubicacion = satelize.satelize({ip:clientIp}, function(err, payload) { return payload.continent.es+','+payload.country.es+','+payload.country_code; });
 
-    var ip = '190.47.115.14';
-    var ubicacion = satelize.satelize({ip:ip}, function(err, payload) { return payload.continent.es+','+payload.country.es+','+payload.country_code; });
-    console.log(ubicacion);
+        var user = new User({
+            nombres:    req.body.nombres,
+            correo:     req.body.correo,
+            fecha:      moment().format(),
+            ip:         clientIp,
+            ubicacion:  ubicacion,
+            suscripcion:true
+        });
 
-    var user = new User({
-        nombres:    req.body.nombres,
-        correo:     req.body.correo,
-        fecha:      moment().format(),
-        ip:         ip,
-        ubicacion:  ubicacion,
-        suscripcion:true
-    });
+        user.save(function(err){
+            return res.status(200).jsonp(user);
+        });
+    } catch (e) {
+        console.log(e);
+        return res.status(500).jsonp({ok:false});
+    }
 
-    user.save(function(err){
-        return res.status(200).jsonp(user);
-    });
+
 };

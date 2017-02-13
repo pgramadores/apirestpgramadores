d0dbe23c96c5dce818ec2316dc77b199a48c9b2c
Juan Mora <raicerk@gmail.com>
2017-02-13 00:20:30 -0300

    Se modifica controller para pruebas de obtencion de ip de cliente

--- controller.js
+++ controller.js
@@ -1,18 +1,26 @@
 var mongoose = require('mongoose');
 var User = mongoose.model('usuarios');
 var satelize = require('satelize');
+var moment = require('moment');
 
 exports.registro = function(req, res) {
 
-    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
+    console.log(req.headers['x-forwarded-for']);
+    console.log(req.connection.remoteAddress);
+    console.log(req.socket.remoteAddress);
+    //190.47.115.14
 
-    var user = new usuarios({
+    var ip = '190.47.115.14';
+    var ubicacion = satelize.satelize({ip:ip}, function(err, payload) { return payload.continent.es+','+payload.country.es+','+payload.country_code; });
+    console.log(ubicacion);
+
+    var user = new User({
         nombres:    req.body.nombres,
         correo:     req.body.correo,
         fecha:      moment().format(),
         ip:         ip,
-        ubicacion:  satelize.satelize({ip:ip}, function(err, payload) { return payload.country.en; }),
-        suscripcion:req.body.suscripcion
+        ubicacion:  ubicacion,
+        suscripcion:true
     });
 
     user.save(function(err){

a88d1eb5fa6b7c1421ad84cbd5837a145624c4f6
Juan Mora <raicerk@gmail.com>
2017-02-18 13:02:51 -0300

    Se agregan nuevas validaciones de seguridad, se agrega mas documentación al codigo

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-// Importamos nuestros modelos,
+// Importación de nuestro modelos
 require('./models/usuarios');
 
-// server.js
+// Inyección de dependencias
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
@@ -9,6 +9,8 @@ var cors = require('cors');
 var helmet = require('helmet');
 var moment = require('moment');
 var session = require('cookie-session');
+var frameguard = require('frameguard');
+var hsts = require('hsts');
 var ctrl = require('./controller');
 var config = require('./config');
 
@@ -16,6 +18,19 @@ var app = express();
 
 // Configuramos Express
 app.use(helmet());
+
+// Seguridad para no permitir el uso del sitio con iframe
+app.use(frameguard({ action: 'deny' }));
+
+// Seguridad para el uso exclusivo de protocolo https para la llamada a la api
+// Solicitar validación en sitio https://hstspreload.org
+app.use(hsts({
+    maxAge: 10886400,
+    includeSubDomains: true,
+    preload: true
+}))
+
+// Configuración de cookies seguras
 app.use(session({
     name: config.nombresession,
     keys: [config.llaveseguridadcookie],
@@ -27,6 +42,7 @@ app.use(session({
         }
     })
 );
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
@@ -34,17 +50,19 @@ app.set('port', config.puerto);
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', 'http://'+config.domain);
     res.setHeader('Access-Control-Allow-Methods', 'POST');
+    res.setHeader('Content-Type','application/json');
     next();
 });
 
 // Iniciamos las rutas de nuestro servidor/API
 var router = express.Router();
 
+// Ruta de bienvenida
 router.get('/', function(req, res) {
-   res.send("Bienvenido a la API REST Pro-Gramadores");
+   res.send({'Mensaje':'Bienvenido a la API REST Pro-Gramadores'});
 });
 
-// Rutas de autenticación y login
+// Rutas de registro
 router.post('/usuarios/registro', ctrl.registro);
 
 app.use(router);
@@ -52,8 +70,11 @@ app.use(router);
 // Iniciamos el servidor y la base de datos
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://'+config.domain+'/pgramadores', function(err) {
-    // Comprobar errores siempre
-    app.listen(app.get('port'), function(){
-        console.log('Express corriendo en http://'+config.domain+':'+config.puerto);
-    });
+    if (!err) {
+        app.listen(app.get('port'), function(){
+            console.log('Express corriendo en http://'+config.domain+':'+config.puerto);
+        });
+    }else{
+        console.log(err.message);
+    }
 });

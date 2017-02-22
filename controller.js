9777601fa1de713b759b8c9bec932cf869f2dc34
Diego Riquelme Murua <driquelmemurua@gmail.com>
2017-02-21 23:39:53 -0300

    correccion error git

--- controller.js
+++ controller.js
@@ -7,7 +7,6 @@ exports.registro = function(req, res) {
 
     try {
 
-
         //prevencion de ataques
         if(serv.Sanar(req.body.correo) && serv.Sanar(req.body.nombres)){
 
@@ -31,6 +30,7 @@ exports.registro = function(req, res) {
                             return res.status(500).jsonp({ok:false});
                         }
                         serv.InvitacionSlack(req.body.correo);
+                        serv.InvitacionMeetup(req.body.correo);
                         return res.status(200).jsonp({ok:true});
                 });
 
@@ -39,26 +39,6 @@ exports.registro = function(req, res) {
             //intento de ataque
             return res.status(403).jsonp({ok:false});           
         }
-        
-
-
-        var clientIp = config.dev ? '190.47.115.14': serv.IP(req);
-
-        var user = new User({
-            nombres:    serv.Sanar(req.body.nombres),
-            correo:     serv.Sanar(req.body.correo),
-            fecha:      serv.Ahora(),
-            ip:         clientIp,
-            ubicacion:  serv.UbicacionPorIp(clientIp),
-            suscripcion:true
-        });
-
-        user.save(function(err){
-            serv.InvitacionSlack(serv.Sanar(req.body.correo));
-            serv.InvitacionMeetup(serv.Sanar(req.body.correo));
-            return res.status(200).jsonp({ok:true});
-        });
-
 
     }catch (e) {
         console.log(e);

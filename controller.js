22ac167d20ec22f175a31f12b6409c3c84a338f3
Diego Riquelme Murua <driquelmemurua@gmail.com>
2017-02-21 23:36:47 -0300

    correccion error git

--- controller.js
+++ controller.js
@@ -7,7 +7,7 @@ exports.registro = function(req, res) {
 
     try {
 
-<<<<<<< HEAD
+
         //prevencion de ataques
         if(serv.Sanar(req.body.correo) && serv.Sanar(req.body.nombres)){
 
@@ -41,7 +41,7 @@ exports.registro = function(req, res) {
         }
         
 
-=======
+
         var clientIp = config.dev ? '190.47.115.14': serv.IP(req);
 
         var user = new User({
@@ -58,7 +58,7 @@ exports.registro = function(req, res) {
             serv.InvitacionMeetup(serv.Sanar(req.body.correo));
             return res.status(200).jsonp({ok:true});
         });
->>>>>>> 6f9a7921c17d6b7ab4d7ed877a1f2dfc2afa8e19
+
 
     }catch (e) {
         console.log(e);
